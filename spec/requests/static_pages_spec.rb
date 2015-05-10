78b2378282f5e74d53f0b20d99de0d8258ec1873
flo <flo.liang@my.com>
2015-05-10 15:00:02 -0700

    Add title pages and test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,15 @@ RSpec.describe "StaticPages", type: :request do
       visit '/static_pages/home'
       expect(page).to have_content('Daily Agenda')
     end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Daily Agenda")
+    end
+
+    it "should no have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 end
