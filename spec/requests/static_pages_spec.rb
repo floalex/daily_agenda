073ec436b889cded40aada4ff95b605f488e1d4e
flo <flo.liang@my.com>
2015-05-13 20:16:58 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,20 +3,30 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
     
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the content 'Daily Agenda" do
-      visit '/static_pages/home'
       expect(page).to have_content('Daily Agenda')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
       expect(page).to have_title("Daily Agenda")
     end
 
     it "should no have a custom page title" do
-      visit '/static_pages/home'
       expect(page).not_to have_title('| Home')
+    end
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it "should have the content 'Help' " do
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title" do
+      expect(page).to have_title('Help')
     end
   end
 end
