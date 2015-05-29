44dea4a2096454101c7f4a52a279cc1fc998dce1
flo <flo.liang@my.com>
2015-05-28 23:42:40 -0700

    Complete seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528150930) do
+ActiveRecord::Schema.define(version: 20150529045155) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150528150930) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_send_at"
+    t.boolean  "admin",             default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+User.create!(name:  "Example User",
+             email: "user@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+5.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  name = Faker::Name.name
+  users.each { |user| user.items.create!(name: name) }
+end
+ 
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Item.count} items created"
+
