0b141133cfb479e49ac1f9f5494d9fdcd0cec610
flo <flo.liang@my.com>
2015-05-30 10:30:37 -0700

    display days left

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
+
+  def days_left
+    (DateTime.now.to_date - created_at.to_date).to_i
+  end
 end
